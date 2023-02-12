b93897f6288ea88d51f89da7b0b1e8af5b3fc43b
Boshi <8356799+Boxis@users.noreply.github.com>
2023-02-11 19:10:35 -0500

    Add 08 calculator solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return  a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  array.forEach(element => {
+    sum+=element;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((a,b) => a*b, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  if (num == 1 || num == 0) return 1;
+
+  for (var i = num - 1; i >= 1; i--) {
+    num = num * i;
+  }
+  return num;
 	
 };
 
