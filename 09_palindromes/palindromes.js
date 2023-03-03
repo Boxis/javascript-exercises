142d8f8ad5de486279131919cdbdbee65be9c1df
Boshi <8356799+Boxis@users.noreply.github.com>
2023-03-03 15:55:35 -0500

    Add solution for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    // Remove punctuations
+    const punctuationless = input.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").toLowerCase();
 
+    // Remove spaces
+    const punctuationless1 = punctuationless.split(' ').join('');
+    console.log(punctuationless1);
+    for (let i = 0; i < punctuationless1.length/2; i++) {
+        if (punctuationless1[i] != punctuationless1[punctuationless1.length - i - 1]) {
+            return false;
+        } 
+    }
+    return true;
 };
 
 // Do not edit below this line
