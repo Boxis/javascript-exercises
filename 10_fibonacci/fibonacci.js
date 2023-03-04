6711fd5ab6cde89a4a372f817ac0226c894b97ed
Boshi <8356799+Boxis@users.noreply.github.com>
2023-03-04 17:52:27 -0500

    Add solution to 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    // shows number of iterations based on provided number
 
+    // new iteration is the sum of the previouss one
+
+    // ex when 3 -> 1, 1, 2
+
+    // ex when 4 -> 1, 1, 2, 3
+
+    let first_num = 1;
+    let second_num = 0;
+    let curr_num = 0;
+    if (number < 0) {
+        return "OOPS";
+    }
+
+    for (let i = 0; i < number; i++) {
+        curr_num = second_num + first_num;
+        // console.log(curr_num);
+        first_num = second_num;
+        second_num = curr_num;
+    }
+    return curr_num;
 };
 
 // Do not edit below this line
