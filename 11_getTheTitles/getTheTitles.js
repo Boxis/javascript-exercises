6d21d2fdede6771a93ebc4802ae8787383df2f37
Boshi <8356799+Boxis@users.noreply.github.com>
2023-03-05 12:44:14 -0500

    Change solution for 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,9 +1,10 @@
 const getTheTitles = function(books) {
-    let result = [];
-    for (arr in books) {
-        result.push(books[arr].title);
-    }
-    return result;
+    // let result = [];
+    // for (arr in books) {
+    //     result.push(books[arr].title);
+    // }
+    // return result;
+    return books.map(book => book.title);
 };
 
 // Do not edit below this line
