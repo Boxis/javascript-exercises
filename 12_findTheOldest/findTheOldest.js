c0f5f31fc17d7c987bec19dacc4b18edcb41ea63
Boshi <8356799+Boxis@users.noreply.github.com>
2023-03-11 19:44:40 -0500

    Add solution to 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const date = new Date();
+    console.log(date);
 
+    const oldest = array.sort(function(a, b) {
+        // Test to see if there are any undefined values, set to today's date
+        if(a.yearOfDeath == undefined || b.yearOfDeath == undefined) {
+            a.yearOfDeath = date;
+            b.yearOfDeath = date;
+        };
+
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
+        return lastGuy > nextGuy ? -1 : 1;
+    }, 0);
+    console.table(oldest);
+    
+    return oldest[0];
 };
 
 // Do not edit below this line
